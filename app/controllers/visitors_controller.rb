37432860d9da605d86b105936d9716373148a0d7
letoan <letranthetoan@gmail.com>
2017-03-01 20:15:38 +0700

    impliment feature_alphabetical

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,9 @@
 class VisitorsController < ApplicationController
   def index
-    @products = Product.all
+  	@products = Product.all.order
+  	if params[:sort]=='SortProducts'
+    @products = @products.order('name desc')
+	end
   end
+
 end
