e3c7346ee1b3fa566d55e9f378f3330848fda118
quan <thanhquan.ng@gmail.com>
2017-03-01 20:35:21 +0700

    M4 done

--- spec/features/sort_products_spec.rb
+++ spec/features/sort_products_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.feature "SortProducts", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+
+  def prod_links
+    page.all(:css, "h4.product-name a").map(&:text)
+  end
+
+  scenario "Click on sort  by Alphabetical" do
+    Product.create!(name: "A")
+    Product.create!(name: "B")
+    Product.create!(name: "C")
+
+    visit root_path
+
+    expect(prod_links).to eq ["A", "B", "C"]
+
+    click_link "Sort by Alphabetical"
+
+    expect(current_url).to eq root_url(sort: "alphabetical")
+    expect(prod_links).to eq ["A", "B", "C"]
+  end
 end
